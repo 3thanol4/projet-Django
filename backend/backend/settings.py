5e86b832557e0cbf3476d529fb273cf2abab6397
3thanol <155687754+3thanol4@users.noreply.github.com>
2026-03-24 11:34:47 +0000

    Remove secret key from settings.py
    Removed the secret key from settings for security reasons.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(=vgbs^2t9oc^6wo@n4nq0_l75wl-x*5d040r$#^3wvr6jo)cl'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
